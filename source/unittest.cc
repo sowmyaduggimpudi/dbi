60d1a34f82bd3dfed70eaf5011b71a49393d5d72
sowmyaduggimpudi <sowmi@ufl.edu>
2015-02-07 20:38:16 -0500

    unittest: Add initial tests

--- source/unittest.cc
+++ source/unittest.cc
@@ -2,13 +2,38 @@
 #include <limits.h>
 #include "gtest/gtest.h"
 #include "DBFile.h"
-
+#include <iostream>
 using namespace std;
+#include <sys/stat.h>
 
-class DBFileUnitTest: public :: testing :: Test {
-public:
-	int p;
-};
+static char *tbl_fpath = "test_data/lineitem.tbl";
+static char *bin_fpath = "test_data/unittest.bin";
+static char *binm_fpath = "test_data/unittest.bin.metadata";
+static Schema mySchema ("catalog", "lineitem");
+static DBFile dbfile;
+TEST(DBFileUnitTest, check_create) {
+	struct stat buf;
+	EXPECT_EQ(true, dbfile.Create(bin_fpath, heap, NULL));
+	dbfile.Load (mySchema, tbl_fpath);
+	dbfile.Close ();
+}
 
-TEST(DBFileUnitTest, check_something) {
+TEST(DBFileUnitTest, check_bin_file) {
+	struct stat buf;
+	dbfile.Create(bin_fpath, heap, NULL);
+	dbfile.Load (mySchema, tbl_fpath);
+	dbfile.Close ();
+	cout<<"FILE EXIT:"<<stat(bin_fpath, &buf) <<endl;
+	
+       	EXPECT_EQ(0, stat(bin_fpath, &buf));
+}
+
+TEST(DBFileUnitTest, check_bin_metafile) {
+	struct stat buf;
+	dbfile.Create(bin_fpath, heap, NULL);
+	dbfile.Load (mySchema, tbl_fpath);
+	dbfile.Close ();
+	cout<<"FILE EXIT:"<<stat(bin_fpath, &buf) <<endl;
+
+       	EXPECT_EQ(0, stat(binm_fpath, &buf));
 }
